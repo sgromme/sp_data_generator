5f2db92a44af9c00ccc864df251f39acef5de8fe
tgromme3 <tgromme3@gatech.edu>
2025-05-14 10:02:42 -0400

    Added functionality to create .csv files from the information stored in a dict of dataframes

--- gen.py
+++ gen.py
@@ -17,5 +17,10 @@ dataset = generator.generate_full_dataset(
     start_date='2023-01-01'
 )
 
+for key in dataset.keys():
+    key_str = str(key) + ".csv"
+    print(key_str)
+    df = dataset[key]
+    df.to_csv(key_str, index=True)
 # Export to Excel for easy viewing or sharing
 generator.export_to_excel(dataset, 'my_supply_chain_data.xlsx')
