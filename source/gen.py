3d7d168579a013d52b2fab65d219f45ff1cae4e0
sgromme <wills1377@gmail.com>
2025-09-09 11:26:35 -0400

    New data generation.

--- source/gen.py
+++ source/gen.py
@@ -3,7 +3,7 @@
 
 
 
-from supply_planning_data_generator import SupplyPlanningDataGenerator
+from supply_planning_data_generator_v2 import SupplyPlanningDataGenerator
 
 # Initialize the generator (optionally with a seed for reproducibility)
 generator = SupplyPlanningDataGenerator(seed=42)
@@ -11,15 +11,14 @@ generator = SupplyPlanningDataGenerator(seed=42)
 # Generate a complete dataset
 dataset = generator.generate_full_dataset(
     num_products=20,
-    num_facilities=5,
-    periods=52,
+    num_facilities=6,
+    periods=104,  # e.g., weeks in 2 years
+    capacity_variation = 0,
     frequency='W',  # Weekly data
     start_date='2023-01-01'
 )
 
-for key in dataset.keys():
-    key_str = str(key) + ".csv"
-    df = dataset[key]
-    df.to_csv(key_str, index=True)
+
 # Export to Excel for easy viewing or sharing
-generator.export_to_excel(dataset, 'my_supply_chain_data.xlsx')
+generator.export_to_excel(dataset, "my_supply_chain_data.xlsx")
+generator.export_to_csv_directory(dataset)
